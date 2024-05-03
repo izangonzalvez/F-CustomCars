b6d1fda1069cf18be5f9cab8b42e2f941d8c3b58
Izan <izgoar@fp.insjoaquimmir.cat>
2024-05-03 19:51:07 +0200

    login register

--- src/widgets/layout/navbar.jsx
+++ src/widgets/layout/navbar.jsx
@@ -1,176 +1,68 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { Link, useNavigate } from "react-router-dom";
-import {
-  Navbar as MTNavbar,
-  MobileNav,
-  Typography,
-  Button,
-  IconButton,
-} from "@material-tailwind/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import { useDispatch, useSelector } from "react-redux";
-import { setUser } from "@/slices/auth/authSlice";
+import { Button } from "@material-tailwind/react";
+import { useSelector, useDispatch } from "react-redux";
 import { doLogout } from "@/slices/auth/thunks";
+import { setAuthToken, setRoles, setUser } from "@/slices/auth/authSlice";
 
 export function Navbar({ brandName, routes, action }) {
-  const [openNav, setOpenNav] = React.useState(false);
-  let [ roles, setRoles ] = useState([]);
   const navigate = useNavigate();
-    
-  const { usuari,authToken } = useSelector (state => state.auth)
-  const dispatch = useDispatch() 
 
-  const getUser = async (e) => {
-    try {
-      const data = await fetch("http://127.0.0.1:8000/api/user", {
-        headers: {
-          Accept: "application/json",
-          Authorization: `Bearer ${authToken}`,
-        },
-        method: "GET",
-      })
-      const resposta = await data.json();
-      if (resposta.success === true) {
-       
-        dispatch(setUser(resposta.user.email));
-        setRoles(resposta.roles);
-      }else{
-        console.log("La resposta no ha triomfat");
-      }            
-    } catch {
-      console.log("Error");
-    }
-  };
+  const { usuari, authToken } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
 
-  useEffect(()=>{
-    getUser();
-  }, []) 
-
-  React.useEffect(() => {
-    window.addEventListener(
-      "resize",
-      () => window.innerWidth >= 960 && setOpenNav(false)
-    );
+  useEffect(() => {
+    dispatch(setAuthToken(localStorage.getItem('authToken')))
+    dispatch(setUser(localStorage.getItem('user')))
+    console.log(authToken)
+    console.log(usuari)
   }, []);
-
-  const navList = (
-    <ul className="mb-4 mt-2 flex flex-col gap-2 text-inherit lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-      {routes.map(({ name, path, icon, href, target }) => (
-        <Typography
-          key={name}
-          as="li"
-          variant="small"
-          color="inherit"
-          className="capitalize"
-        >
-          {href ? (
-            <a
-              href={href}
-              target={target}
-              className="flex items-center gap-1 p-1 font-bold"
-            >
-              {icon &&
-                React.createElement(icon, {
-                  className: "w-[18px] h-[18px] opacity-75 mr-1",
-                })}
-              {name}
-            </a>
-          ) : (
-            <Link
-              to={path}
-              target={target}
-              className="flex items-center gap-1 p-1 font-bold"
-            >
-              {icon &&
-                React.createElement(icon, {
-                  className: "w-[18px] h-[18px] opacity-75 mr-1",
-                })}
-              {name}
-            </Link>
-          )}
-        </Typography>
-      ))}
-    </ul>
-  );
 
   const logout = (e) => {
     e.preventDefault();
     dispatch(doLogout());
-    navigate("/")
-};
+    console.log(authToken)
+    navigate("/");
+  };
+
   return (
-    <MTNavbar color="transparent" className="p-3">
-      <div className="container mx-auto flex items-center justify-between text-white">
-        <Link to="/" className="flex items-center">
-          <img src="../../../public/img/Coches.png" alt="Logo" className="mr-2 w-20 h-100" />
-          <Typography className="mr-4 ml-2 cursor-pointer py-1.5 font-bold">
-            {brandName}
-          </Typography>
-        </Link>
-        <div className="hidden lg:block">{navList}</div>
-        <div className="hidden gap-2 lg:flex">
-          <a href="https://www.material-tailwind.com/blocks?ref=mtkr" target="_blank"/>
-          {/* {React.cloneElement(action, {
-            className: "hidden lg:inline-block",
-          })} */}
-        </div>
-        <div>
-              { usuari } <a>(<span></span> { roles.map (  (v)=> (
-                            <span key={v}> {v} </span>
-                        ) ) })</a>
-            ( 
-              <a href=""></a>
-              <a className="text-orange-800" onClick={logout} href="">Logout</a>)
-              
+    <>
+      <nav className="px-4 p-4">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center">
+            <Link to="/">
+              <img
+                src="../../../public/img/Coches.png"
+                className="w-20 h-20"
+                alt="Brand Logo"
+              ></img>
+            </Link>
+            <div className="pl-9 text-white ">
+              <Link to="/project" className="mr-4">Proyectos </Link>
+              <Link to="/community" className="mr-4">Comunidad </Link>
+              <Link to="/chat" className="mr-4">Chat </Link>
+              <Link to="/contact" className="mr-4">Contacto </Link>
+              <Link to="/prueba" className="mr-4">Prueba </Link>
+            </div>
           </div>
-        <IconButton
-          variant="text"
-          size="sm"
-          color="white"
-          className="ml-auto text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-          onClick={() => setOpenNav(!openNav)}
-        >
-          {openNav ? (
-            <XMarkIcon strokeWidth={2} className="h-6 w-6" />
+          {authToken ? (
+            <div>
+              <a className="mr-4 text-white">{usuari}</a>
+              <Button color="red" onClick={logout}>
+                Logout
+              </Button>
+            </div>
           ) : (
-            <Bars3Icon strokeWidth={2} className="h-6 w-6" />
+            <div>
+              <Link to="/login" className="mr-4 text-white">Login</Link>
+              <Link to="/register" className="mr-4 text-white">Register</Link>
+            </div>
           )}
-          
-        </IconButton>
-      </div>
-      <MobileNav
-        className="rounded-xl bg-white px-4 pt-2 pb-4 text-blue-gray-900"
-        open={openNav}
-      >
-        <div className="container mx-auto">
-          {navList}
-          <a href="https://www.material-tailwind.com/blocks/react?ref=mtkr" target="_blank" className="mb-2 block" />
         </div>
-        <div>
-              { usuari } <a>(<span></span> { roles.map (  (v)=> (
-                            <span key={v}> {v} </span>
-                        ) ) })</a>
-            ( 
-              <a href=""></a>
-              <a className="text-orange-800" onClick={logout} href="">Logout</a>)
-              
-          </div>
-      </MobileNav>
-    </MTNavbar>
+      </nav>
+    </>
   );
 }
-
-Navbar.defaultProps = {
-  brandName: "CustomCars Luxury",
-};
-
-Navbar.propTypes = {
-  brandName: PropTypes.string,
-  routes: PropTypes.arrayOf(PropTypes.object).isRequired,
-  action: PropTypes.node,
-};
-
-Navbar.displayName = "/src/widgets/layout/navbar.jsx";
 
 export default Navbar;
